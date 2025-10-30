048509e4488333cdbb3e5ca31f2e1f9672db63d7
zetvies <zetvies@gmail.com>
2025-10-30 10:37:28 +0700

    use new marker

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/qr.mind',
+      imageTargetSrc: './assets/targets/slow.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
